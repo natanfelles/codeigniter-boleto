92ebafd1d065a7e353d7b88c215320df35ca07e1
Natan Felles <natanfelles@gmail.com>
2016-04-08 23:44:44 -0300

    Add Boleto PHP

---
+++ application/views/boleto.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * CodeIgniter Boleto
+ *
+ * @package    CodeIgniter Boleto
+ * @author     Natan Felles
+ * @link       https://github.com/natanfelles/codeigniter-boleto
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
